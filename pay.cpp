51f9853c009fa45a105a8bd8db1c2a5db012633e
Joshua Yun <joshuayun1999@gmail.com>
2019-03-07 17:09:41 -0800

    Finished the whole Assignentb

--- pay.cpp
+++ pay.cpp
@@ -9,32 +9,90 @@
 #include<string>
 #include<vector>
 #include<stdio.h>
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cstring>
+#include<string>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
 
 void print(vector<Person> &employees);
 void readData(vector<Person> &employees);
-void writeData();
-void printHighestPaid();
-void separateAndSave();
-void getCompanies(vector<Person> &employees,
-  vector<string> &companyNames)
+void printHighestPaid(vector<Person> &employees);
+void separateAndSave(vector<Person> &employees, vector<string> &companyNames);
+void getCompanies(vector<Person> &employees, vector<string> &companyNames);
 
-void separateAndSave()
+void separateAndSave(vector<Person> &employees, vector<string> &companyNames)
 {
-
+  ofstream output;
+  // string name;
+  for (int i = 0; i < companyNames.size(); i++)
+  {
+    float total = 0;
+    //name = companyNames.at(i) + ".txt";
+    output.open(companyNames.at(i) + ".txt");
+    for (int z = 0; z < employees.size(); z++)
+    {
+      if (companyNames.at(i) == employees.at(z).getCompanyName())
+      {
+        output << setw(16) << left << employees.at(z).getFirstName();
+        output << setw(16) << left << employees.at(z).getLastName();
+        output << setw(16) << left << employees.at(z).getEmployeeId();
+        output << setw(16) << left << employees.at(z).getCompanyName();
+        output << "$" << setw(16) << left << fixed << setprecision(2) << employees.at(z).totalPay() << endl;
+        total = total + employees.at(z).totalPay();
+      }
+    }
+    output << "Total: $" << fixed << setprecision(2) << total;
+    output.close();
+  }
 }
 
-void printHighestPaid()
+void printHighestPaid(vector<Person> &employees)
 {
-
+  int number;
+  float number1;
+  int i = 0;
+  while(i < employees.size())
+  {
+    if (number1 < employees.at(i).totalPay())
+    {
+      number1 = employees.at(i).totalPay();
+      number = i;
+    }
+    i++;
+  }
+  cout << "Highest Paid: " << employees.at(number).fullName() << endl;
+  cout << "Employee ID: " << employees.at(number).getEmployeeId() << endl;
+  cout << "Employer: " << employees.at(number).getCompanyName() << endl;
+  cout << "Total Pay: $" << fixed << setprecision(2) << employees.at(number).totalPay() << endl;
 }
 void getCompanies(vector<Person> &employees, vector<string> &companyNames)
 {
   for (int i = 0; i < employees.size(); i++)
   {
-    companyNames.at(i) = employees.at(i).getCompanyName();
+    int z=0;
+    if (companyNames.empty())
+    {
+      companyNames.push_back(employees.at(i).getCompanyName());
+    }
+    for (int x = 0; x < companyNames.size(); x++)
+    {
+      if (employees.at(i).getCompanyName() == companyNames.at(x))
+      {
+        z++;
+      }
+    }
+    if(z == 0)
+    {
+      companyNames.push_back(employees.at(i).getCompanyName());
+    }
   }
 }
+
 
 void readData(vector<Person> &employees)
 {
@@ -71,7 +129,7 @@ int main()
   vector<string> companyNames;
   readData(employees);
   getCompanies(employees, companyNames);
-  printHighestPaid();
-  separateAndSave();
+  printHighestPaid(employees);
+  separateAndSave(employees, companyNames);
   return 0;
 }
